7a2c1508018459c2381c87c13b70d00fc0259465
kcprater <kristopherprater@gmail.com>
2020-03-27 08:22:13 -0400

    1

--- src/Components/Fetch.js
+++ src/Components/Fetch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nasa from './Nasa';
+import Nasa from './Nasa/Nasa'
 import Weather from './Weather';
 import Zomato from './Zomato';
 
@@ -8,15 +8,11 @@ export default class Fetch extends React.Component {
         super(props)
 
         this.state = {
-            location: '',
-            lat: '',
-            long: '' 
-        }
+            location: {lat: 0, long: 0}
+        };
     }
 
-    componentDidMount() {
-        console.log('component mounted');
-
+    fetchLocation = () => {
         fetch('http://ip-api.com/json')
             .then(response => response.json())
             .then(json => {
@@ -28,9 +24,13 @@ export default class Fetch extends React.Component {
                 })
                 console.log(this.state.location);
                 console.log(this.state.lat);
-                console.log(this.state.lon);
+                console.log(this.state.long);
             })
-    };
+    }
+
+    componentDidMount() {
+        this.fetchLocation();
+        }
 
     render(){
         return(
@@ -38,8 +38,8 @@ export default class Fetch extends React.Component {
                 <h1>24 Hour Project</h1>
 
                 <Nasa lat={this.state.lat} long={this.state.long} location={this.state.city} />
-                <Weather lat={this.state.lat} long={this.state.long} location={this.state.city} />
-                <Zomato lat={this.state.lat} long={this.state.long} location={this.state.city} />
+                <Weather lat={this.state.lat} long={this.state.lon} location={this.state.city} />
+                <Zomato lat={this.state.lat} long={this.state.lon} location={this.state.city} />
 
             </div>
         )
